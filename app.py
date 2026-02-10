e50bdd9a8e7caa4cc2c020f6d3dbf7d22d61dba7
Manya1131 <manyagarg565@gmail.com>
2026-02-10 12:23:51 +0530

    fix render port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import google.generativeai as genai
 app = Flask(__name__)
 
@@ -130,4 +131,5 @@ def bfhl():
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
